d1f3f8450557af5f47d6b243d49fd22760d54e98
ingrafa <38500731+ingrafa@users.noreply.github.com>
2021-01-11 14:15:06 -0500

    Estudiante1

---
+++ src/main/java/UPS/Logica.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UPS;
+
+/**
+ *
+ * @author rafik
+ */
+public class Logica {
+    
+ public String metodoIntegrante1(){
+     return "Integrante1";
+ }
+public String metodoIntegrante2(){
+     return "Integrante2";
+ }
+public String metodoIntegrante3(){
+     return "Integrante3";
+ }
+    
+}
